03265c176f6d303f4e152683115bc80e50cdeec5
Nicholas Valbusa <squallstar@gmail.com>
2019-10-17 20:03:52 +0200

    refactored init

--- js/build.js
+++ js/build.js
@@ -1,24 +1,12 @@
-// Include your namespaced libraries
 var Notifications = new Fliplet.Registry.get('fliplet-widget-notifications:1.0:core');
 
-(function () {
-  var notifications;
+Fliplet.Widget.instance('fliplet-widget-notifications-1-0-0', function (data) {
+  var options = {};
 
-  // This function will run for each instance found in the page
-  Fliplet.Widget.instance('fliplet-widget-notifications-1-0-0', function (data) {
-    // Sample implementation to initialize the widget
-    notifications = new Notifications(data);
-  });
-
-  Fliplet.Widget.register('Notifications', function () {
-    return notifications;
-  });
-
-  Fliplet.Hooks.run('beforeNotificationsInit')
-    .then(notifications.init)
-    .catch(function (err) {
-      if (err) {
-        console.warn(err);
-      }
+  Fliplet.Hooks.run('notificationsSetup', data).then(function () {
+    Fliplet.Hooks.run('beforeNotificationsInit', data, options).then(function () {
+      var notifications = new Notifications(data);
+      notifications.init(options);
     });
-})();
+  });
+});
